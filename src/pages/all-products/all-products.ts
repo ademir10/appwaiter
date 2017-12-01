695bb3a8ee8971cef9cc21fec0da2e5a9e49ae4d
ademir10 <ddsuportservice@gmail.com>
2017-12-01 14:39:10 -0200

    teste

---
+++ src/pages/all-products/all-products.ts
@@ -0,0 +1,189 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, LoadingController, AlertController, NavParams,  App } from 'ionic-angular';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/toPromise';
+//para guardar o id da mesa aberta localmente
+import { NativeStorage } from '@ionic-native/native-storage';
+
+@IonicPage()
+@Component({
+  selector: 'page-all-products',
+  templateUrl: 'all-products.html',
+})
+export class AllProductsPage {
+  //o titulo usado na view
+  public title_desk;
+  responseData : any;
+  //pega o nome da categoria que está vindo da outra view
+  var_category_id: string = this.navParams.get('id_categoria');
+  var_category_name: string = this.navParams.get('nome_categoria');
+  var_products: string = this.navParams.get('produtos_categoria');
+  private enderecoApi: string = "http://192.168.0.37:3000/";
+  //private enderecoApi: string = "http://dsoft.ddns.net:37000/";
+  public all_products: any;
+  public id_aberto: any;
+  public id_produto: any;
+  public formas_de_pagamento: any;
+  public currentNumber = 1;
+  public observacao = '';
+  constructor(
+     public navCtrl: NavController,
+     public navParams: NavParams,
+     public alertCtrl: AlertController,
+     public http: Http,
+     public app: App,
+     public loadingCtrl: LoadingController,
+     //para armazenar o id da mesa localmente
+     private nativeStorage: NativeStorage) {
+      console.log('AO CARREGAR A VIEW : ', this.var_products);
+      this.initializeItems();
+   }
+
+   private increment () {
+     this.currentNumber++;
+   }
+   private decrement () {
+       if (this.currentNumber > 1) {
+       this.currentNumber--;
+     }
+   }
+
+   initializeItems() {
+       this.all_products = this.var_products;
+     }
+
+ add_product(data:any) {
+       //loading
+      let loading = this.loadingCtrl.create({
+      spinner: 'hide',
+      content: 'Carregando aguarde…'
+      });
+      loading.present();
+      //enquanto é lido o conteudo o loading fica na tela
+      setTimeout(() => {
+        //pega o id do produto selecionado na view
+        this.id_produto = data.id_produto
+        this.nativeStorage.getItem('current_session').then
+            ((dados_mesa_aberta) =>
+            {
+             if (data)
+               {
+                 let headers = new Headers({ 'Content-Type' : 'application/json'});
+                 let options = new RequestOptions({ headers: headers });
+                 //here we send the data to API
+                 let data = JSON.stringify({
+                 cardToken: 'G0d1$@Bl3T0d0W4Th3V3Rth1Ng',
+                 desk_order_id: dados_mesa_aberta.id_da_mesa,
+                 product_id: this.id_produto,
+                 qnt_product: this.currentNumber,
+                 qrpoint_name: dados_mesa_aberta.qr_code_mesa,
+                 obs_prod: this.observacao,
+                 });
+
+                 return new Promise((resolve, reject) => {
+                   this.http.post(this.enderecoApi + 'add_product', data, options)
+                   .toPromise()
+                   .then((response) => {
+                     const retorno_da_API = response.json();
+                     this.currentNumber = 1;
+                     const alert = this.alertCtrl.create({
+                     subTitle: retorno_da_API.produto_adicionado.name + ' adicionado!',
+                     buttons: ['OK'],
+                     });
+                     alert.present();
+                    this.observacao = '';
+                     console.log('API Response : ', response.json());
+                     resolve(response.json());
+                   })
+                   .catch((error) =>
+                   {
+                     console.error('API Error : ', error.status);
+                     console.error('API Error : ', JSON.stringify(error));
+                     reject(error.json());
+
+                     const alert = this.alertCtrl.create({
+                     title: 'Falha de conexão:',
+                     subTitle: 'Desculpe, não conseguimos encontrar o servidor, avise o suporte.',
+                     buttons: ['OK'],
+                     });
+                     alert.present();
+
+                   });
+                 });
+
+              }
+              else
+              {
+                this.nativeStorage.setItem('current_session', '');
+                this.id_aberto = '';
+              }
+            }), error => {
+              console.error('Ocorreu um erro : ', error.status);
+            };
+      });
+
+      setTimeout(() => {
+      loading.dismiss();
+      });
+
+
+ }
+      //exibe os dados já consumidos
+      check_consumacao() {
+          //loading
+          let loading = this.loadingCtrl.create({
+          spinner: 'hide',
+          content: 'Carregando aguarde…'
+            });
+            loading.present();
+            //enquanto é lido o conteudo o loading fica na tela
+            setTimeout(() => {
+              this.nativeStorage.getItem('current_session').then
+                  ((dados_mesa_aberta) =>
+                  {
+                       let headers = new Headers({ 'Content-Type' : 'application/json'});
+                       let options = new RequestOptions({ headers: headers });
+                       //here we send the data to API
+                       let data = JSON.stringify({
+                       cardToken: 'G0d1$@Bl3T0d0W4Th3V3Rth1Ng',
+                       desk_order_id: dados_mesa_aberta.id_da_mesa,
+                       });
+
+                       return new Promise((resolve, reject) => {
+                         this.http.post(this.enderecoApi + 'check_order', data, options)
+                         .toPromise()
+                         .then((response) => {
+                           const retorno_da_API = response.json();
+                           this.navCtrl.push('CheckOrderPage',{ desk_name: retorno_da_API.mesa_venda, items: retorno_da_API.items_venda, total_geral: retorno_da_API.total_geral, formas_de_pagamento: retorno_da_API.formas_pagamento });
+                           console.log('API Response : ', response.json());
+                           resolve(response.json());
+                         })
+                         .catch((error) =>
+                         {
+                           console.error('API Error : ', error.status);
+                           console.error('API Error : ', JSON.stringify(error));
+                           reject(error.json());
+
+                           const alert = this.alertCtrl.create({
+                           title: 'Falha de conexão:',
+                           subTitle: 'Desculpe, não conseguimos encontrar o servidor, avise o suporte.',
+                           buttons: ['OK'],
+                           });
+                           alert.present();
+
+                         });
+                       });
+
+                  }), error => {
+                    console.error('Ocorreu um erro : ', error.status);
+                  };
+            });
+
+            setTimeout(() => {
+              loading.dismiss();
+            });
+
+      }
+}
---
+++ src/pages/menu/menu.ts
@@ -0,0 +1,205 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, AlertController, LoadingController, NavParams,  App } from 'ionic-angular';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/toPromise';
+//para guardar o id da mesa aberta localmente
+import { NativeStorage } from '@ionic-native/native-storage';
+
+@IonicPage()
+@Component({
+  selector: 'page-menu',
+  templateUrl: 'menu.html',
+})
+export class MenuPage {
+  //o titulo usado na view
+  title = 'Categorias';
+  private categories_api: any;
+  responseData : any;
+  products : any;
+  //envia o id da categoria para a API para trazer os produtos da catagoria
+  public id_category;
+  public name_category;
+  private enderecoApi: string = "http://192.168.0.37:3000/";
+  //private enderecoApi: string = "http://dsoft.ddns.net:37000/";
+  public all_categories: any; // <- esta variavel é a responsável em levar os dados para a view
+  public id_categoria: string;
+  public nome_categoria: string;
+  public produtos_categoria: any;
+  public formas_de_pagamento: any;
+  constructor(
+     public navCtrl: NavController,
+     public navParams: NavParams,
+     public alertCtrl: AlertController,
+     public http: Http,
+     private loadingCtrl: LoadingController,
+     public app: App,
+     //para armazenar o id da mesa localmente
+     private nativeStorage: NativeStorage) {
+
+      let loadingPopup = this.loadingCtrl.create({
+        content: 'Loading...'
+      });
+      this.http.get(this.enderecoApi + '/list_categories').subscribe(data => { this.categories_api = data.json().categories_product;
+        console.log('RETORNO DO GET : ', data.json().categories_product);
+      this.initializeItems();
+      // Hide the loading message
+      loadingPopup.dismiss();
+          });
+  }
+
+  initializeItems() {
+      this.all_categories = this.categories_api;
+    }
+
+    getItems(ev: any) {
+        // Reset items back to all of the items
+        this.initializeItems();
+
+        // set val to the value of the searchbar
+        let val = ev.target.value;
+
+        // if the value is an empty string don't filter the items
+        if (val && val.trim() != '') {
+          this.all_categories = this.all_categories.filter((t) => {
+            return (t.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+          })
+        }
+      }
+
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MenuPage');
+  }
+
+  //TRÁS TODOS OS PRODUTOS DA CATEGORIA SELECIONADA
+  chama_produtos(data:any) {
+    //loading
+    let loading = this.loadingCtrl.create({
+    spinner: 'hide',
+    content: 'Carregando aguarde...'
+      });
+      loading.present();
+      //enquanto é lido o conteudo o loading fica na tela
+      setTimeout(() => {
+        //aproveita o nome da categoria e manda para view que exibira os produto
+        this.id_category = data.category_id;
+        this.name_category = data.category_name;
+        this.navCtrl.push('AllProductsPage',{id_categoria: data.category_id, nome_categoria: data.category_name});
+      });
+      setTimeout(() => {
+        loading.dismiss();
+      });
+
+    }
+
+  category_products (data:any){
+    //loading
+    let loading = this.loadingCtrl.create({
+    spinner: 'hide',
+    content: 'Carregando aguarde...'
+      });
+    loading.present();
+    //enquanto é lido o conteudo o loading fica na tela
+    setTimeout(() => {
+      this.id_category = data.category_id;
+      this.name_category = data.category_name;
+      let headers = new Headers(
+      {
+        'Content-Type' : 'application/json'
+      });
+      let options = new RequestOptions({ headers: headers });
+      //here we send the data to API
+      let dados = JSON.stringify({
+        cardToken: 'G0d1$@Bl3T0d0W4Th3V3Rth1Ng',
+        id_da_categoria: data.category_id
+      });
+      //aqui é feito o post enviando os parametros para a API
+      return new Promise((resolve, reject) => {
+        this.http.post(this.enderecoApi + '/list_products', dados, options)
+        .toPromise()
+        //aqui o retorno da API é carregado em Json em um array
+        .then((response) => {
+          this.products = response.json();
+
+          this.navCtrl.push('AllProductsPage',{id_categoria: data.category_id, nome_categoria: data.category_name, produtos_categoria: this.products});
+          console.log('API Response : ', response.json());
+          resolve(response.json());
+        })
+        .catch((error) =>
+        {
+          console.error('API Error : ', error.status);
+          console.error('API Error : ', JSON.stringify(error));
+          reject(error.json());
+
+          const alert = this.alertCtrl.create({
+          title: 'Falha de conexão:',
+          subTitle: 'Desculpe, não conseguimos encontrar o servidor, avise o suporte.',
+          buttons: ['OK'],
+          });
+          alert.present();
+        });
+      });
+    });
+    setTimeout(() => {
+      loading.dismiss();
+    });
+ }
+
+  //exibe os dados já consumidos
+  check_consumacao() {
+        //loading
+      let loading = this.loadingCtrl.create({
+      spinner: 'hide',
+      content: 'Carregando aguarde…'
+    });
+    loading.present();
+    //enquanto é lido o conteudo o loading fica na tela
+    setTimeout(() => {
+      this.nativeStorage.getItem('current_session').then
+          ((dados_mesa_aberta) =>
+          {
+               let headers = new Headers({ 'Content-Type' : 'application/json'});
+               let options = new RequestOptions({ headers: headers });
+               //here we send the data to API
+               let data = JSON.stringify({
+               cardToken: 'G0d1$@Bl3T0d0W4Th3V3Rth1Ng',
+               desk_order_id: dados_mesa_aberta.id_da_mesa,
+               });
+               return new Promise((resolve, reject) => {
+                 this.http.post(this.enderecoApi + 'check_order', data, options)
+                 .toPromise()
+                 .then((response) => {
+                   const retorno_da_API = response.json();
+                   this.navCtrl.push('CheckOrderPage',{ desk_name: retorno_da_API.mesa_venda, items: retorno_da_API.items_venda, total_geral: retorno_da_API.total_geral, formas_de_pagamento: retorno_da_API.formas_pagamento });
+                   console.log('API Response : ', response.json());
+                   resolve(response.json());
+                 })
+                 .catch((error) =>
+                 {
+                   console.error('API Error : ', error.status);
+                   console.error('API Error : ', JSON.stringify(error));
+                   reject(error.json());
+
+                   const alert = this.alertCtrl.create({
+                   title: 'Falha de conexão:',
+                   subTitle: 'Desculpe, não conseguimos encontrar o servidor, avise o suporte.',
+                   buttons: ['OK'],
+                   });
+                   alert.present();
+
+                 });
+               });
+
+          }), error => {
+            console.error('Ocorreu um erro : ', error.status);
+          };
+    });
+
+    setTimeout(() => {
+      loading.dismiss();
+    });
+
+  }
+}
